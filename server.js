1b4fcd2c3ce2ef737c2745b3847d6f0b7179e6f1
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-20 22:06:05 -0400

    added get request for route note page and home page, and add listen for port number

--- server.js
+++ server.js
@@ -2,11 +2,24 @@
 const express = require("express");
 const fs = require("fs");
 const uuid = require("./helpers/uuid");
+const path = require("path");
 
 //Start the express server 
 const app = express();
 const PORT = 3001;
 
+//Express server to read from public directory
+app.use(express.static('public'));
+
 //Parse data 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//GET request for html route notes page 
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+
+//GET request for html route to home page 
+app.get("*", (req, res) => res.sendFile(path_join(__dirname, "public/index.html")));
+
+//Listening to the port number 
+app.listen(PORT, () => console.log(`Application listening to PORT http://localhost:${PORT}`));
